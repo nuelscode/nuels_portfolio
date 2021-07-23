16c337e46da6fb8119092d028a50220246510582
nuelscode <chinagorom.onwubiko@gmail.com>
2021-07-23 13:15:55 -0700

    NavBar Completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className="container">
+
+  <a className="navbar-brand" href="#">MyLogo</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <FontAwesomeIcon icon={faBars}  style={{ color: "#fff" }}/>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav ms-auto" >
+      <li className="nav-item ">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Experience</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact Me</a>
+      </li>
+
+    </ul>
+  </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
